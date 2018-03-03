2c142d8ec2a3f7eb4d117524d751b199c79a25d9
rpdemilt <rpdemilt@cpp.edu>
2018-03-03 11:24:45 -0800

    Tracking Algorithm
    the robot now has an algorithm it uses to lead shots

--- GunTest.java
+++ GunTest.java
@@ -2,52 +2,77 @@ package robots;
 
 import java.awt.Color;
 import robocode.*;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+import static robocode.util.Utils.normalRelativeAngle;
 
 public class GunTest extends RateControlRobot{
 	private int turnSwitch = 1;
+	private int gunTurnSwitch = 1;
+	private int turnRate = 5;
 	private int clock = 0;
+	private int pattern = 0;
+	
 	public void run() {
 		setBodyColor(Color.RED);
 		setGunColor(Color.BLUE);
 		setRadarColor(Color.WHITE);
+		setBulletColor(Color.RED);
 		
-		setAdjustRadarForRobotTurn(false);
-		setAdjustGunForRobotTurn(false);
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(false);
 		
-		setRadarRotationRateRadians(Math.PI / 4.0);
-		setGunRotationRateRadians(Math.PI / 4.0);
-		execute();
-		while(true) {	
-			if(clock < 8) {
-				setTurnRate(turnSwitch * 10);
-				setVelocityRate(50);
-			} else if(clock > 8) {
-				setTurnRate(turnSwitch * -10);
-				setVelocityRate(50);
+		addCustomEvent(new Condition("HighEnergy") {
+			public boolean test() {
+				return getEnergy() > 80;
+			}
+		});
+		addCustomEvent(new Condition("LowEnergy") {
+			public boolean test() {
+				return getEnergy() > 30;
+			}
+		});
+		addCustomEvent(new Condition("Clock") {
+			public boolean test() {
+				return clock >= 8;
+			}
+		});
+		while(true) {
+			setGunRotationRate(5 * gunTurnSwitch);
+			switch(pattern) {
+				case 0:
+					setVelocityRate(10);
+					setTurnRate(turnSwitch * turnRate);
+					break;
+				case 1:
+				case 2:
+				default:
 			}
 			execute();
 		}
 	}
 	public void onScannedRobot(ScannedRobotEvent e) {
+		setGunRotationRate(0);
+		execute();
 		if(e.getBearing() >= 0) {
-			turnSwitch = 1;
+			gunTurnSwitch = 1;
 		} else {
-			turnSwitch = -1;
+			gunTurnSwitch = -1;
+		}
+		double targetBearing = getHeading() + e.getBearing();
+		double targetVelocity = e.getVelocity();
+		double bearingFromGun = normalRelativeAngleDegrees(targetBearing - getGunHeading());
+		if(targetVelocity == 0) {
+			if(bearingFromGun <= 10) {
+				if(canFire()) {
+					fire(3);
+				}
+			} else {
+				setTurnGunRight(getHeading() + bearingFromGun);
+			}
+		} else {
+			leadShot(getProjectileSpeed(e.getDistance()),e);
 		}
 		
-		if(getEnergy() > 50) {
-			if(getGunHeat() == 0) {
-				fire(1);
-			}
-			ahead(e.getDistance() + 5);
-			scan();
-		} else {
-			back(e.getDistance());
-			if(getGunHeat() == 0) {
-				fire(.5);
-			}
-			turnRight(-turnSwitch * 5);
-		}
 	}
 	public void onHitRobot(HitRobotEvent e) {
 		if(e.getBearing() >= 0) {
@@ -62,5 +87,42 @@ public class GunTest extends RateControlRobot{
 	}
 	public void onHitWall(HitWallEvent e) {
 		setVelocityRate(-1 * getVelocityRate());
+	}
+	public void onCustomEvent(CustomEvent e) {
+		if(e.getCondition().getName().equals("LowEnergy")) {
+			
+		} else if(e.getCondition().getName().equals("Clock")) {
+			if(pattern == 2) {
+				pattern = 0;
+			} else {
+				pattern++;
+			}
+		}
+	}
+	private boolean canFire() {
+		return getGunHeat() == 0;
+	}
+	private void leadShot(double projectileSpeed, ScannedRobotEvent e) {
+		double x = getX();
+		double y = getY();
+		double distance = e.getDistance();
+		double targetVelocity = e.getVelocity();
+		double targetHeading = e.getHeading();
+		double time = projectileSpeed * distance;
+		
+		double nextX = x + distance * Math.sin(targetHeading) + targetVelocity * Math.cos(targetHeading) * time;
+		double nextY = y + distance * Math.cos(targetHeading) + targetVelocity * Math.sin(targetHeading) * time;
+		
+		double trackAngle = Math.atan(nextX/nextY);
+		turnGunRightRadians(normalRelativeAngle(trackAngle - getGunHeadingRadians()));
+		if(canFire()) {
+			fire(getFirePower(distance));
+		}
+	}
+	private double getProjectileSpeed(double distance) {
+		return 20 - getFirePower(distance) * 3 ;
+	}
+	private double getFirePower(double distance) {
+		return Math.min(400 / distance,3);
 	}
 }
